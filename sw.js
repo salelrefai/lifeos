dff0721a646b6bfac2f0cb756cdb28b8d021abcf
Salma <salma@Salmas-MBP.home>
2026-03-29 21:31:50 +0200

    Updates

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'lifeos-v1';
+const urlsToCache = [
+  '/lifeos/',
+  '/lifeos/index.html',
+  '/lifeos/static/js/main.js',
+  '/lifeos/static/css/main.css',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      if (response) return response;
+      return fetch(event.request).catch(() => caches.match('/lifeos/'));
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
+```
+
+Save with `Cmd + S`. Now let's deploy. In your terminal run:
+```
+npm run deploy
